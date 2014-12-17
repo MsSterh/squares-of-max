d7ddd6cc51021a1f29e188f7a81d38d94b66ba0b
Andrey Bakuta <anb@anahoret.com>
2014-12-17 16:42:33 +0200

    Bolierplate code.

--- squaresOfMax.js
+++ squaresOfMax.js
@@ -1,17 +1,5 @@
-function sq(a) { return a * a } 
-
 function squaresOfMax(a, b, c) {
-  var res;
-
-  if (a >= b && b >= c) {
-    res = sq(a) + sq(b);
-  } else if (a <= b && b <= c) {
-    res = sq(b) + sq(c);
-  } else {
-    res = squaresOfMax(c, a, b);
-  }
-
-  return res;
+  // Your code goes here
 }
 
 module.exports = squaresOfMax;
