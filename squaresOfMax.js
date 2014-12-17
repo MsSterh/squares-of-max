c51978b4dc07d8325adc89a45a3eb5142b64f55b
Andrey Bakuta <anb@anahoret.com>
2014-12-17 16:24:04 +0200

    Try travis tests.

--- squaresOfMax.js
+++ squaresOfMax.js
@@ -1,7 +1,17 @@
 function sq(a) { return a * a } 
 
 function squaresOfMax(a, b, c) {
-  // Your code goes here
+  var res;
+
+  if (a >= b && b >= c) {
+    res = sq(a) + sq(b);
+  } else if (a <= b && b <= c) {
+    res = sq(b) + sq(c);
+  } else {
+    res = squaresOfMax(c, a, b);
+  }
+
+  return res;
 }
 
 module.exports = squaresOfMax;
